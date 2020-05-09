57762967086bc71771446150609aa71d68f8e9f5
demonofthemist <rohan.misal8@gmail.com>
2020-05-09 22:04:02 +0530

    nextjs removed

--- components/Section/index.js
+++
@@ -1,9 +0,0 @@
-import './styles.scss'
-
-export default function Section({ children, style }) {
-  return (
-    <section className="section" style={{ ...style }}>
-      <div className="section__content">{children}</div>
-    </section>
-  )
-}
--- out/_next/static/chunks/styles.bc526b318d013603379d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"00et":function(n,o,p){},CMpC:function(n,o,p){},LV1p:function(n,o,p){}}]);
